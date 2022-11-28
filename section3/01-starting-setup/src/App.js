fed7f0e9f38374af55c8c2d93834fbec9e8b4958
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-28 08:12:23 -0800

    Day 2 - Section 3 - Playing with components

--- section3/01-starting-setup/src/App.js
+++ section3/01-starting-setup/src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started, Beaux-migo!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
