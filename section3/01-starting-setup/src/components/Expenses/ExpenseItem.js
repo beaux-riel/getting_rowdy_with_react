28db0f12ed25d7101df8fdc4498ea26902a6dfb4
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-30 13:53:36 -0800

    Section 4 - useState hooks

--- section3/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ section3/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,14 @@
-import React from'react';
+import React, { useState } from'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
     const clickHandler = () => {
-        console.log("Clicked");
+        setTitle("Purchased!");
+        console.log(title);
     };
     // const expenseDate = new Date(2021, 2, 28);
     // const expenseTitle = 'Car Insurance';
@@ -15,10 +18,10 @@ const ExpenseItem = (props) => {
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={clickHandler}>Add to cart</button>
         </Card>
     );
 }
