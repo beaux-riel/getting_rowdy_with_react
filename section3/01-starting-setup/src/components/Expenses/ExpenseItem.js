a1af03c3c7a8f2ebeb9307fa050215009f0189e6
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-30 08:30:49 -0800

    Day 3 - Section 4 - React State and Working with Events

--- section3/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ section3/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log("Clicked");
+    };
     // const expenseDate = new Date(2021, 2, 28);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.67;
@@ -15,6 +18,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
