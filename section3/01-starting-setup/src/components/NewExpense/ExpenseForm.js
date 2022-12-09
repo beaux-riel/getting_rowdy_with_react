36d092f20bf954a38427301935fbdb48e57f3393
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-08 22:22:00 -0800

    Multistate situations vs single blocks of code.

--- section3/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ section3/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,33 +1,69 @@
+// Old mate has got me importing react to remind me that I'll see it "in the wild"
 import React, { useState } from 'react';
 
+// Obviously importing the old css
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    // Individual state slices/Multistate approach -> Prefer this vs "blocking" everything together.
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    // });
+
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // });
     };
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
     };
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData)
     };
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
                     <input type='text' onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
-                    <label>Amount</label>
+                    <label>Amount - $</label>
                     <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
