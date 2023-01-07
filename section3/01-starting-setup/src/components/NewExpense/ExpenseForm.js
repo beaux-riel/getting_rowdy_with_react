35107f24aaac7870e943d11dab4e05497fa1dab2
beaux <34432201+beaux-riel@users.noreply.github.com>
2023-01-06 20:53:52 -0800

    Passing data: Parent to child -> child to parent

--- section3/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ section3/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -53,9 +53,6 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData)
-        setEnteredTitle('');
-        setEnteredAmount('');
-        setEnteredDate('');
     };
 
     return (
@@ -63,31 +60,15 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input 
-                        type='text' 
-                        value={enteredTitle} 
-                        onChange={titleChangeHandler} 
-                    />
+                    <input type='text' onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount - $</label>
-                    <input 
-                        type='number' 
-                        min="0.01" 
-                        step="0.01"
-                        value={enteredAmount}
-                        onChange={amountChangeHandler} 
-                    />
+                    <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input 
-                        type='date' 
-                        min="2019-01-01" 
-                        max="2023-12-31" 
-                        value={enteredDate}
-                        onChange={dateChangeHandler} 
-                    />
+                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
